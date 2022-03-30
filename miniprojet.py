76f40db29f8f45ae428f3062972902117e6fdbd4
chenetheophile <73079835+chenetheophile@users.noreply.github.com>
2022-03-30 13:46:56 +0100

    scrap 2 sitesBase+csv

--- miniprojet.py
+++ miniprojet.py
@@ -7,67 +7,69 @@ Created on Tue Mar 22 21:45:08 2022
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import time
-import csv
 import pandas as pd
+
 site=['https://aqicn.org/map/france/fr/',"https://e.infogram.com/d106e364-d435-4e45-8a64-e059d8f4d5e8?parent_url=https%3A%2F%2Fwww.automobile-propre.com%2Fdossiers%2Fchiffres-vente-immatriculations-france%2F&src=embed#async_embed"]
 driver = webdriver.Firefox()
 ImmatriculationsElectrique={}
-Pollutiondelair=[]
+Pollutiondelair={}
 for url in site:
     driver.get(url)
     time.sleep(2)
     if url is site[0]:
-        ville=['Paris','Marseille','Lyon','Toulouse','Nice','Nantes','Montpellier','Strasbourg','Bordeaux','Lille','Rennes',
-                'Reims','Toulouse','Saint-Étienne','Le Havre', 'Grenoble', 'Dijon','Angers','Saint-Denis','Villeurbanne',
-                'Nîmes','Clermont-Ferrand','Aix-en-Provence','Le Mans','Brest','Tours','Amiens','Limoges','Annecy',
-                'Boulogne-Billancourt','Perpignan','Metz','Besançon','Orléans','Rouen','Montreuil','Argenteuil',
-                'Mulhouse','Caen',	'Nancy','Saint-Paul','Roubaix','Tourcoing',	'Nanterre','Vitry-sur-Seine','Nouméa',
-                'Créteil','Avignon','Poitiers','Aubervilliers','Asnières-sur-Seine','Aulnay-sous-Bois']
+        # ville=['Paris','Marseille','Lyon','Toulouse','Nice','Nantes','Montpellier','Strasbourg','Bordeaux','Lille','Rennes',
+        #         'Reims','Toulouse','Saint-Étienne','Le Havre', 'Grenoble', 'Dijon','Angers','Saint-Denis','Villeurbanne',
+        #         'Nîmes','Clermont-Ferrand','Aix-en-Provence','Le Mans','Brest','Tours','Amiens','Limoges','Annecy',
+        #         'Boulogne-Billancourt','Perpignan','Metz','Besançon','Orléans','Rouen','Montreuil','Argenteuil',
+        #         'Mulhouse','Caen',	'Nancy','Saint-Paul','Roubaix','Tourcoing',	'Nanterre','Vitry-sur-Seine','Nouméa',
+        #         'Créteil','Avignon','Poitiers','Aubervilliers','Asnières-sur-Seine','Aulnay-sous-Bois']
+        ville=['Paris','Marseille','Lyon Centre','Toulouse','Nice','Nantes','Montpellier','Strasbourg est','Talence','Lille fives','Rennes']
         for nom in ville:
             time1=time.perf_counter()
-            recherche=driver.find_element_by_id('full-page-search-input')
+            recherche=driver.find_element(By.ID,'full-page-search-input')
             time.sleep(2)
             recherche.send_keys(nom)
             time.sleep(3)
             recherche.location_once_scrolled_into_view
+            time.sleep(3)
+            driver.find_element(By.ID,'searchResults').find_element(By.TAG_NAME,'a').click()
             time.sleep(2)
-            driver.find_element_by_id('searchResults').find_element_by_tag_name('a').click()
+            driver.find_element(By.ID,'h1header3').location_once_scrolled_into_view
             time.sleep(2)
-            driver.find_element_by_id('h1header3').location_once_scrolled_into_view
+            donneeshistorique=driver.find_element(By.CLASS_NAME,'historical-yearly-data')
             time.sleep(2)
-            donneeshistorique=driver.find_element_by_class_name('historical-yearly-data')
-            time.sleep(2)
-            Annee=donneeshistorique.find_elements_by_class_name('year-divider')
+            Annee=donneeshistorique.find_elements(By.CLASS_NAME,'year-divider')
             marqueurAnnee=[]
             time.sleep(2)
-            ligneTab=donneeshistorique.find_elements_by_tag_name('tr')
+            ligneTab=donneeshistorique.find_elements(By.TAG_NAME,'tr')
             time.sleep(2)
             for element in Annee:
                 marqueurAnnee.append(element.text)
-            time.sleep(2)
-            annee=[]
-            for i in range(len(ligneTab)):  
+            time.sleep(1)
+            annee={}
+            diction={}
+            for i in range(len(ligneTab)-1,-1,-1):  
                     if ligneTab[i].get_attribute('class')=="year-divider":
-                        print(ligneTab[i].text)
-                        Pollutiondelair.append(annee)
-                        annee=[]
+                        diction[ligneTab[i].text]=(annee)
+                        Pollutiondelair[nom]=diction
+                        annee={}
                     else:
-                        carre=ligneTab[i].find_elements_by_class_name('squares')
+                        mois=[]
+                        carre=ligneTab[i].find_elements(By.CLASS_NAME,'squares')
                         time.sleep(1)
                         for element in carre:
-                            text=element.find_elements_by_tag_name('text')
-                            time.sleep(1)
+                            text=element.find_elements(By.TAG_NAME,'text')
                             for balise in text:
-                                annee.append(balise.text)
+                                mois.append(balise.text)
+                        annee[ligneTab[i].find_element(By.TAG_NAME,'td').text]=mois
             temps=(time.perf_counter()-time1)
-            print('nombre de seconde pour une ville:'+str(temps))
+            print('nombre de seconde pour '+nom+':'+str(temps))
+        pd.DataFrame(Pollutiondelair).to_csv("Pollution.csv",sep=";")
     else:
-        menu=Select(driver.find_element_by_tag_name("select"))
+        annee=[]
+        menu=Select(driver.find_element(By.TAG_NAME,"select"))
         opt=menu.options
         for op in opt:
             annee.append(op.text)
@@ -76,10 +78,10 @@ for url in site:
             valeur={}
             driver.find_element_by_css_selector("div[class='igc-tab-switcher igc-tab-switcher__left']").click()
             time.sleep(5)
-            mois=driver.find_element_by_class_name('igc-graph-group').find_elements_by_tag_name('text')
-            labelMois=driver.find_element_by_class_name('igc-x-axis-text').find_elements_by_tag_name('text')
+            mois=driver.find_element(By.CLASS_NAME,'igc-graph-group').find_elements(By.TAG_NAME,'text')
+            labelMois=driver.find_element(By.CLASS_NAME,'igc-x-axis-text').find_elements(By.TAG_NAME,'text')
             for j in range(len(mois)):
                 valeur[labelMois[j].text]=mois[j].text
             ImmatriculationsElectrique[annee[options-i-1]]=(valeur)
-        df=pd.DataFrame(ImmatriculationsElectrique)
-        df.to_csv('Immat.csv',sep=";",encoding='utf-8')
+        pd.DataFrame(ImmatriculationsElectrique).to_csv('Immat.csv',sep=";",encoding='utf-8')
+        
