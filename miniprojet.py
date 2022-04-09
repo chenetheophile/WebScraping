25ba509b81bf19fc6b50dd05e8431ebc8d481843
chenetheophile <73079835+chenetheophile@users.noreply.github.com>
2022-04-09 19:15:35 +0100

    Merge DataFrame

--- miniprojet.py
+++ miniprojet.py
@@ -98,3 +98,41 @@ for url in site:
                 iter+=1
         df=pd.DataFrame({'Annee':AnneeList,'Region':RegionList,'Ville':VilleList,"Moyenne Annuel":MoyenneList}).to_csv("Pollution2.csv",sep=";")
 
+"""
+
+Partie Merge Donnée
+
+"""
+dfProduction=pd.read_csv("parc-region-annuel-production-filiere.csv",sep=";")
+dfProduction=dfProduction.drop(["Code INSEE région","Géo-shape région","Géo-point région"],axis=1)
+dfProduction=dfProduction.fillna(0)
+
+dfImmatriculations=pd.read_csv("ImmatriculationsTypeEnergieRegion.csv",sep=";")
+print(dfImmatriculations.info())
+
+dfPopulation=pd.read_csv("PopulationRegionAnnee.csv",sep=";")
+
+Population=[]
+
+dfQualite=pd.read_csv("Pollution2.csv",sep=";")
+nomRegion=["Ile-de-France","Centre-Val de Loire","Bourgogne-Franche-Comte","Normandie","Hauts-de-France","Grand Est",
+                "Pays de la Loire","Bretagne","Nouvelle-Aquitaine","Occitanie","Auvergne-Rhone-Alpes","Provence-Alpes-Cote d'Azur","Corse"]
+moyenneRegion=[]
+Region=[]
+listAnnee=[]
+for region in nomRegion:
+    tabAnnee=[]
+    for annee in dfQualite.loc[lambda ligne:ligne['Region']==region]["Annee"]:
+        if annee not in tabAnnee:
+            tabAnnee.append(annee)
+    for element in tabAnnee:
+        listAnnee.append(element)
+        moyenneRegion.append(dfQualite.loc[lambda ligne:ligne['Annee']==element].loc[lambda ligne:ligne['Region']==region,'Moyenne Annuel'].mean())
+        Region.append(region)
+
+df=pd.DataFrame({'Annee':listAnnee,'MoyenneRegion':moyenneRegion,'Region':Region})
+print(df.count())
+df=pd.merge(df,dfPopulation,on=["Annee","Region"],how='inner')
+df=pd.merge(df,dfProduction,on=["Annee","Region"],how='inner')
+df=pd.merge(df,dfImmatriculations,on=["Annee","Region"],how='inner')
+print(df.to_csv("C:/Users\chene\Desktop\Donneefusionner.csv",sep=";"))
